5a9f40dee40a50f6db8ef14d3de902e73e2135cb
likwokbiu <li_bill@ymail.com>
2018-02-21 14:31:07 -0500

    Completed for Feb 21

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,15 +1,12 @@
 class EntriesController < ApplicationController
 
   def index
-    render :index
   end
 
   def show
-    render :show
   end
 
   def new
-    render :new
   end
 
   def create
@@ -17,11 +14,14 @@ class EntriesController < ApplicationController
   end
 
   def edit
+  end
+
+  def update
     redirect_to entry_url(params[:id])
   end
 
   def destroy
-    render :index
+    redirect_to :index
   end
-  
+
 end
