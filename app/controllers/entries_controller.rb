5eb8d060022ce0e127baf6baaf66d89d78dc07a1
likwokbiu <li_bill@ymail.com>
2018-02-22 22:45:16 -0500

    Feb 22

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,27 +1,54 @@
 class EntriesController < ApplicationController
 
   def index
+    @entries=Entry.all
   end
 
   def show
+    @entry=Entry.find(params[:id])
   end
 
   def new
+    @entry=Entry.new
   end
 
   def create
-    redirect_to entries_url
+    @entry = Entry.new
+    @entry.word = params[:entry][:word]
+    @entry.language = params[:entry][:language]
+    @entry.definition = params[:entry][:definition]
+
+    if @entry.save
+      redirect_to entries_path
+    else
+      redirect_to new_entries_path
+    end
+
   end
 
   def edit
+    @entry = Entry.find(params[:id])
   end
 
   def update
-    redirect_to entry_url(params[:id])
+    @entry = Entry.find(params[:id])
+
+    @entry.word = params[:entry][:word]
+    @entry.language = params[:entry][:language]
+    @entry.definition = params[:entry][:definition]
+
+    if @entry.save
+      redirect_to entry_url(@entry)
+    else
+      redirect_to entry_url(@entry)
+    end
+
   end
 
   def destroy
-    redirect_to :index
+    @entry = Entry.find(params[:id])
+    @entry.destroy
+    redirect_to entries_path
   end
 
 end
